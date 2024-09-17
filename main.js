1d3baa2b58f0dd75b99cdc06859cd932a08aa266
tomeytran <tmtri310251@gmail.com>
2024-09-17 13:55:25 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// BAI 1
+let number;
+let total = 0;
+while (number != -1) {
+	number = +prompt("Enter a number: ");
+	alert(number);
+	//phần code tính tổng
+	total += number;
+	8;
+}
+document.write(total);
+
+// BAI 2
+let i = 1;
+while (i < 100) {
+	document.write(`<hr width = ${i}%>`);
+	i++;
+}
